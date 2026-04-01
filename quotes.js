cd71a9d55585e90cbd38432b10c68dfc1d79a49e
Volcanohawaii <ggooggooing@gmail.com>
2026-04-01 21:10:29 +0900

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -35,6 +35,14 @@ const i18n = {
         nextMission: "Future Assignment"
     }
 };
+const elRelation = {
+    "木": { sheng: "火", ke: "土", name: "목(木)" },
+    "火": { sheng: "土", ke: "金", name: "화(火)" },
+    "土": { sheng: "金", ke: "水", name: "토(土)" },
+    "金": { sheng: "水", ke: "木", name: "금(金)" },
+    "水": { sheng: "木", ke: "火", name: "수(水)" }
+onst hanjaMap = { "목(木)": "木", "화(火)": "火", "토(土)": "土", "금(金)": "金", "수(水)": "水" };
+
 /* [필수 데이터: 영문 성함 재료] */
 const nameRootEn = ["Al", "Be", "Ro", "Lu", "Ma", "E", "I", "Ka", "Kh", "Ph", "Ri", "Ni", "San", "Ben", "Pha", "Di", "Le", "Mi", "Nel", "Bel", "An", "El", "O", "U", "Cl", "Fl", "Br", "Te", "St", "Ke", "Me", "Jo", "Je", "Ca", "Pi", "Ba", "Sha", "Ta", "Na", "Whi", "Lin", "Ky", "Da", "On", "Yu", "Jun", "Yun", "Sul", "Ye", "Jin", "Hye"];
 const nameTailEn = ["to", "ra", "na", "a", "ri", "el", "on", "ro", "s", "nel", "d", "k", "te", "sha", "lu", "ni", "bel", "run", "in", "um", "tis", "ti", "na", "sia", "an", "더", "ell", "en", "i", "o", "a", "ta", "카", "de", "le", "mi", "pi", "st", "tr", "반", "sen", "kan", "yan", "lin", "ron", "rill", "lan", "le", "ra", "na", "no"];
